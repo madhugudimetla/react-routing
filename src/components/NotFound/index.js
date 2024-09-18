40f4f1f3d30dce159bb6f9290f4c2d484db4d603
madhu <madhubalu9908@gmail.com>
2024-09-18 22:29:34 +0530

    React routing

---
+++ src/components/NotFound/index.js
@@ -0,0 +1,12 @@
+import { Component } from "react";
+import './index.css'
+class NotFound extends Component{
+    render(){
+        return(
+            <div className="container">
+                <img alt="not found" src="https://assets.ccbp.in/frontend/react-js/not-found-blog-img.png" className="img"/>
+            </div>
+        )
+    }
+}
+export default NotFound
